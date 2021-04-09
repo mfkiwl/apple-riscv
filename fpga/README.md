5dbd96b2279ad0abe6b0be6e98a4e40a8faff75c
Heqing Huang <feipenghhq@gmail.com>
2021-04-08 23:16:42 -0700

    Created makefile based flow for FPGA
        - Setup the tcl file and makefile for running quartus-ii and vivado

---
+++ fpga/README.md
@@ -0,0 +1,18 @@
+# FPGA
+
+## How to run FPGA build
+
+Command to run quartus/vivado
+
+```bash
+cd <intel>/<xilinx>
+
+# to run fpga build on soc level
+make soc
+
+# to run fpga build on core level
+make core
+
+# see help command
+make help
+```
---
+++ fpga/xilinx/run.tcl
@@ -0,0 +1,48 @@
+############################################################
+# Tcl script for vivado
+############################################################
+
+# ========================================
+# Step 1: Create project Design Setup
+# ========================================
+
+set PRJ_NAME $::env(PRJ_NAME)
+set TOP $::env(TOP)
+set DEVICE $::env(DEVICE)
+
+set SOC_RTL_PATH rtl/soc
+set REPO_ROOT [exec git rev-parse --show-toplevel]
+set OUTPUT $PRJ_NAME/output
+#exec mkdir -p $OUTPUT
+
+create_project $PRJ_NAME -dir $PRJ_NAME -part $DEVICE
+
+# ========================================
+# Step 2: Read in source files
+# ========================================
+set SOC_RTL  $REPO_ROOT/$SOC_RTL_PATH/apple_riscv_soc.v
+read_verilog $SOC_RTL
+
+# ========================================
+# Step 3: synthesis
+# ========================================
+synth_design -part $DEVICE -top $TOP
+write_checkpoint -force $OUTPUT/syn/syn_checkpoint
+report_timing_summary -file $OUTPUT/syn/syn_timing.rpt
+report_utilization -file $OUTPUT/syn/syn_utilization_all.rpt
+report_utilization -hierarchical -file $OUTPUT/syn/syn_utilization_hier.rpt
+
+# ========================================
+# Step 4: opt, place and route
+# ========================================
+
+# PNR not supported on the cpu core.
+if {$::env(RUN_APR) == 1} {
+opt_design
+place_design
+phys_opt_design
+write_checkpoint -force $OUTPUT/pnr/pnr_checkpoint
+report_timing_summary -file $OUTPUT/pnr/pnr_timing.rpt
+report_utilization -file $OUTPUT/pnr/pnr_utilization_all.rpt
+report_utilization -hierarchical -file $OUTPUT/pnr/pnr_utilization_hier.rpt
+}
