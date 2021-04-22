db5f5a19322c43b5dda7bc7915e49568ea72a2e3
Heqing Huang <feipenghhq@gmail.com>
2021-04-21 17:28:49 -0700

    Designed a sib switch with 1 to N topology.

--- src/main/scala/sib/sib.scala
+++ src/main/scala/sib/sib.scala
@@ -18,12 +18,13 @@ package sib
 import spinal.core._
 import spinal.lib._
 
-case class SibConfig(addressWidth: Int, dataWidth: Int) {
+case class SibConfig(addressWidth: Int, dataWidth: Int, addr_lo: Int = 0, addr_hi: Int = 1) {
   def addressType  = UInt(addressWidth bits)
   def dataType     = Bits(dataWidth bits)
   def bytePerWord  = dataWidth / 8
   def symboleRange = log2Up(bytePerWord) - 1 downto 0
   def wordRange    = addressWidth - 1 downto log2Up(bytePerWord)
+  assert(addr_lo <= addr_hi, "SIB addr_lo should smaller or equal to addr_hi")
 }
 
 /**
@@ -115,8 +116,12 @@ case class Sib(config: SibConfig) extends Bundle with IMasterSlave {
 
   def >>(that: Sib): Unit = that << this
 
+  /**
+   * Remap a bus
+   */
   def remapAddress(remapping: UInt => UInt): Sib = {
     val address = remapping(this.addr)
+
     val busRemap = Sib(SibConfig(dataWidth = this.config.dataWidth, addressWidth = address.getBitsWidth))
 
     this.resp   := busRemap.resp
@@ -125,11 +130,47 @@ case class Sib(config: SibConfig) extends Bundle with IMasterSlave {
 
     busRemap.sel    := this.sel
     busRemap.enable := this.enable
-    busRemap.addr   := this.addr
+    busRemap.addr   := address
     busRemap.write  := this.write
     busRemap.mask   := this.mask
     busRemap.wdata  := this.wdata
 
     busRemap
+  }
+}
+
+/**
+ * SIB 1 to n switch
+ */
+case class Sib_switch1tN(mainSibCfg: SibConfig, targetSibCfg: Array[SibConfig]) extends Component {
+
+  // defining the interface
+  val hostSib = slave(Sib(mainSibCfg))
+  val clientSib = targetSibCfg map (x => master(Sib(x)))
+
+  // check if the switch is valid
+  assert(mainSibCfg.addressWidth >= targetSibCfg.map(_.addressWidth).max,
+    "SIB: The main module address range is smaller then the target module")
+  assert(mainSibCfg.dataWidth >= targetSibCfg.map(_.dataWidth).max,
+    "SIB: The main module data range is smaller then the target module")
+
+  val num = targetSibCfg.length
+  val dec_sel = Bits(num bits)
+  val dec_good = dec_sel.orR
+  for (i <- 0 until num) {
+    dec_sel(i) := (hostSib.addr >= clientSib(i).config.addr_lo) & (hostSib.addr <= clientSib(i).config.addr_hi)
+  }
+
+  hostSib.rdata := MuxOH(dec_sel, clientSib.map(_.rdata))
+  hostSib.resp  := MuxOH(dec_sel, clientSib.map(_.resp)) & dec_good
+  hostSib.ready := MuxOH(dec_sel, clientSib.map(_.ready))
+
+  for (i <- 0 until num) {
+    clientSib(i).write  := hostSib.write
+    clientSib(i).addr   := hostSib.addr(clientSib(i).config.addressWidth - 1 downto 0)
+    clientSib(i).wdata  := hostSib.wdata
+    clientSib(i).enable := hostSib.enable
+    clientSib(i).mask   := hostSib.mask
+    clientSib(i).sel    := hostSib.sel & dec_sel(i)
   }
 }
