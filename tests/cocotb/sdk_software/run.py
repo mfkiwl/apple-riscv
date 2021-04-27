42d02fbee1937680c730f81ca5a21e7be89ab48b
Heqing Huang <feipenghhq@gmail.com>
2021-04-26 18:37:56 -0700

    Added cocotb test frame work for sdk software

---
+++ tests/cocotb/sdk_software/run.py
@@ -0,0 +1,107 @@
+##################################################################################################
+##
+## Copyright 2021 by Heqing Huang (feipenghhq@gamil.com)
+##
+## ~~~ Hardware in SpinalHDL ~~~
+##
+## Author: Heqing Huang
+## Date Created: 04/06/2021
+##
+## ================== Description ==================
+##
+## Test using riscv-test
+##
+##################################################################################################
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import FallingEdge, Timer
+
+import os
+import subprocess
+
+subprocess_run = subprocess.Popen("git rev-parse --show-toplevel", shell=True, stdout=subprocess.PIPE)
+subprocess_return = subprocess_run.stdout.read()
+REPO_ROOT = subprocess_return.decode().rstrip()
+
+###############################
+# Common function
+###############################
+
+def process_rom_file(name, main):
+    """ Split the text and data section for the generated verilog file """
+
+    # Link the instruction rom file to the tb directory
+    SRC_FILE = REPO_ROOT + '/sdk/software/' + name + '/' + main + ".verilog"
+    ROM_FILE = os.getcwd() + '/verilog.rom' # need to link the instruction ram file the the current directory
+    if os.path.isfile(ROM_FILE):
+        os.remove(ROM_FILE)
+    os.symlink(SRC_FILE, ROM_FILE)
+
+    FP = open('verilog.rom', "r")
+    IRAM_FP = open('instr_ram.rom', "w")
+    DRAM_FP = open('data_ram.rom', "w")
+
+    iram = True
+    for line in FP.readlines():
+        if line.rstrip() == "@01000000":
+            iram = False
+            continue
+        if iram:
+            IRAM_FP.write(line)
+        else:
+            DRAM_FP.write(line)
+
+
+    FP.close()
+    IRAM_FP.close()
+    DRAM_FP.close()
+
+def print_register(dut, size=32):
+    """ Print the register value """
+    for i in range(size):
+        try:
+            val = dut.DUT_apple_riscv_soc.cpu_core.regfile_inst.ram[i].value.integer
+        except ValueError:
+            val = 'X'
+        print(f"Register {i}, {hex(val)}")
+
+def check_register(dut, expected):
+    """ Check the register file with the expected data """
+    for key, value in expected.items():
+        val = dut.DUT_apple_riscv_soc.cpu_core.regfile_inst.ram[key].value.integer
+        assert value == val, f"RAM1: Register {key}, Expected: {value}, Actual: {val}"
+        print(f"RAM1: Register {key}, Expected: {value}, Actual: {val}")
+
+async def reset(dut, time=20):
+    """ Reset the design """
+    dut.reset = 1
+    await Timer(time, units="ns")
+    await FallingEdge(dut.clk)
+    dut.reset = 0
+
+
+
+
+###############################
+# Test suites
+###############################
+
+@cocotb.test()
+async def run_test(dut):
+    runtime = int(os.getenv('RUN_TIME'))
+    name = os.getenv('PROGRAM')
+    top = os.getenv('PROGRAM_TOP')
+    process_rom_file(name, top)
+    clock = Clock(dut.clk, 10, units="ns")  # Create a 10us period clock on port clk
+    cocotb.fork(clock.start())  # Start the clock
+    await reset(dut)
+    await Timer(runtime, units="ns")
+    # This pattern is defined in the TEST_PASS macro
+    expected_register = {
+        1 : 1,
+        2 : 2,
+        3 : 3,
+    }
+    print_register(dut, 32)
+    check_register(dut, expected_register)
---
+++ tests/cocotb/sdk_software/run_all.py
@@ -0,0 +1,192 @@
+#!/usr/bin/python3
+##################################################################################################
+##
+## Copyright 2021 by Heqing Huang (feipenghhq@gamil.com)
+##
+## ~~~ Hardware in SpinalHDL ~~~
+##
+## Author: Heqing Huang
+## Date Created: 04/06/2021
+##
+## ================== Description ==================
+##
+## script to run all the instructions
+##
+##################################################################################################
+
+import os
+import subprocess
+import shutil
+
+#####################################
+# ISA
+#####################################
+
+# isa and its run time
+rv32ui_isa = [
+    ['add'   , 5000],
+    ['addi'  , 3000 ],
+    ['and'   , 10000],
+    ['andi'  , 3000 ],
+    ['auipc'  , 3000 ],
+    ['beq'  , 5000 ],
+    ['bge'  , 5000 ],
+    ['bgeu'  , 5000 ],
+    ['blt'  , 5000 ],
+    ['bltu'  , 5000 ],
+    ['bne'  , 5000 ],
+    #['fence_i'  , 3000 ],
+    ['jal'  , 3000 ],
+    ['jalr'  , 3000 ],
+    ['lb'   , 3000],
+    ['lbu'  , 3000 ],
+    ['lh'   , 3000],
+    ['lhu'  , 3000 ],
+    ['lui'  , 3000 ],
+    ['lw'  , 3000 ],
+    ['or'   , 10000],
+    ['ori'  , 3000 ],
+    ['sb'   , 5000],
+    ['sh'  , 10000 ],
+    ['simple', 2000 ],
+    ['sll'   , 10000],
+    ['slli'  , 3000 ],
+    ['slt'   , 5000],
+    ['slti'  , 3000 ],
+    ['sltu'   , 5000],
+    ['sltiu'  , 3000 ],
+    ['sra'   , 10000],
+    ['srai'  , 3000 ],
+    ['srl'   , 10000],
+    ['srli'  , 10000 ],
+    ['sub'   , 5000],
+    ['sw'  , 10000 ],
+    ['xor'   , 10000],
+    ['xori'  , 3000 ],
+]
+
+rv32mi_isa = [
+    ['mcsr', 3000],
+    ['ma_addr', 10000],
+	['illegal', 10000]
+]
+
+rv32si_isa = [
+    ['csr', 6000],
+    ['scall', 6000],
+]
+
+soc_isa = [
+    ['software_interrupt', 6000],
+    ['timer_interrupt', 6000],
+]
+
+# architecture
+ARCH = {
+    'rv32ui': rv32ui_isa,
+    'rv32mi': rv32mi_isa,
+    'rv32si': rv32si_isa,
+    'soc': soc_isa
+}
+
+#####################################
+# Common variable
+#####################################
+
+OUTPUT_DIR  = 'output'
+
+FILES  = [
+    'DUT_apple_riscv_soc.vcd',
+    'results.xml',
+]
+
+RESULT = 'results.xml'
+
+#####################################
+# Utility function
+#####################################
+
+def clear_all():
+    """ Clear all the output """
+    if os.path.isdir(OUTPUT_DIR):
+        shutil.rmtree(OUTPUT_DIR)
+    os.system("make clean_all")
+
+def move_result(test, arch):
+    """ Move the test result to its own directory """
+    path = OUTPUT_DIR + '/' + arch + '/' + test
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    for file in FILES:
+        tgt = path + '/' + file
+        shutil.move(file, tgt)
+
+def run_test(test, arch, runtime):
+    """ invoke makefile to run a test """
+    cmd = f'make TESTNAME={test} RISCVARCH={arch} RUNTIME={runtime}'
+    os.system(cmd)
+
+def check_result():
+    """ Check the test result """
+    with open(RESULT) as file:
+        contents = file.read()
+        search_word = "<failure />"
+        file.close()
+        return not (search_word in contents)
+
+def one_test(test, arch, runtime):
+    """ run all the process for one tests """
+    os.system("make clean_rom")
+    run_test(test, arch, runtime)
+    result = check_result()
+    move_result(test, arch)
+    return result
+
+def one_arch_tests(arch):
+    """ run all tests in an arch """
+    tests = ARCH[arch]
+    results = {}
+    for test, runtime in tests:
+        result = one_test(test, arch, runtime)
+        results[test] = result
+    return results
+
+def all_arch_tests(archs):
+    results = {}
+    for arch in archs.keys():
+        result = one_arch_tests(arch)
+        results[arch] = result
+    return results
+
+def print_result(results):
+    translate = {
+        True: "PASS",
+        False: "FAIL"
+    }
+    final_pass = True
+    print("=======================================")
+    print("              Tests Result             ")
+    print("=======================================")
+    for arch in results.keys():
+        print("## ISA: " + arch + " ##")
+        tests = results[arch]
+        for test in tests.keys():
+            rst = translate[tests[test]]
+            final_pass = final_pass & tests[test]
+            print(test + ": " + str(rst), end='')
+            if rst == 'PASS':
+                print()
+            else:
+                print('    <---- ' + test + ' ---->')
+    if final_pass:
+        print("Congratulation!! All tests PASS")
+    else:
+        print("Some tests FAILED")
+
+def main():
+    """ Run all the test """
+    clear_all()
+    results = all_arch_tests(ARCH)
+    print_result(results)
+
+main()
